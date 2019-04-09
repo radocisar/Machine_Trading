6047275c1316b752b247dc92924386174fdf581a
radocisar <radocisar@hotmail.com>
2019-04-08 20:01:52 -0400

    Working on BB strategy

--- Raw_ticks/Bollinger_Band_Strategy.vb
+++ Raw_ticks/Bollinger_Band_Strategy.vb
@@ -4,10 +4,14 @@
     Public upper_band As Double
     Public lower_band As Double
     Public middle_band As Double
+    Public Upper_Lower_Band_Span As Double
 
     Sub Bollinger_Band_Trading_Strategy(candle_arr() As Candle, open_price As Double, high_price As Double, low_price As Double, last_price As Double) Handles Candles_creation_data_processing_event.start_trading_strategy
 
-
+        upper_band =
+        lower_band =
+        middle_band =
+        Upper_Lower_Band_Span = 
 
 
 
